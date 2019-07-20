5406346d3c02451c9d2a1cf4dc70f907bd39b2aa
Robert Glyde <robert.glyde@sky.uk>
2019-07-20 20:03:20 +0100

    updated docker config

--- project/DockerSettings.scala
+++ project/DockerSettings.scala
@@ -3,12 +3,10 @@ import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport._
 import sbt.Keys._
 
 object DockerSettings {
-
-  val dockerRepo = "r-glyde"
   
   lazy val common = Seq(
     dockerBaseImage := "openjdk:8u201-alpine",
-    dockerRepository := Some(dockerRepo),
+    dockerRepository := Some("glyderj"),
     dockerLabels := Map("maintainer" -> "r-glyde"),
     dockerUpdateLatest := true,
     packageName in Docker := s"krapi/${name.value}"
